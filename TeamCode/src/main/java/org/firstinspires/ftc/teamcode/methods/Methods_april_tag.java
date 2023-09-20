23957cf38fdb3ea54f57b2bab8c419218d7bc69d
glevse <bloxore@gmail.com>
2023-09-20 16:57:48 +0300

    start

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/methods/Methods_april_tag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/methods/Methods_april_tag.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.methods;
 //Todo: include there all required for April tag detection instead of creating methods in opmodes classes for vanya
 public class Methods_april_tag {
+
+
 }//alo
