5a65e87310c61043c68f2851fd99c21851cb5b33
glevse <bloxore@gmail.com>
2023-10-05 12:32:23 +0300

    update robot controller

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTagSwitchableCameras.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptAprilTagSwitchableCameras.java
@@ -50,13 +50,12 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: AprilTag Switchable Cameras", group = "Concept")
-
+@Disabled
 public class ConceptAprilTagSwitchableCameras extends LinearOpMode {
 
     /*
      * Variables used for switching cameras.
      */
-
     private WebcamName webcam1, webcam2;
     private boolean oldLeftBumper;
     private boolean oldRightBumper;
