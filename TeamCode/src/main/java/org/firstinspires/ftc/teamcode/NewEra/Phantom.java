8c0a57ca4363ef511032cf8a31f391f95e28ac3e
Hkial <bloxore@gmail.com>
2024-04-03 23:39:27 +0300

    Finished camera setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEra/Phantom.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEra/Phantom.java
@@ -6,6 +6,7 @@ import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
@@ -21,7 +22,7 @@ public class Phantom extends LinearOpMode {
     public TfodProcessor tfodProcessor;
     public AprilTagProcessor aprilTagProcessor;
     public VisionPortal visionPortal;
-    public DcMotor leftF, rightF, leftB, rightB, pod, actu , zx, pnap;
+    public DcMotorEx leftF, rightF, leftB, rightB, pod, actu , zx, pnap;
     public CRServo zaxvat, pisun, big, zaxvatLeft, zaxvatRight, bros, kr, psk;
     private FtcDashboard dash = FtcDashboard.getInstance();
     public BNO055IMU imu;
@@ -43,12 +44,12 @@ public class Phantom extends LinearOpMode {
     }
 
     public void hardwareMapGetter(){
-        leftF = hardwareMap.dcMotor.get("lf");
-        leftB = hardwareMap.dcMotor.get("lr");
-        rightF = hardwareMap.dcMotor.get("rf");
-        rightB = hardwareMap.dcMotor.get("rr");
+        leftF = hardwareMap.get(DcMotorEx.class, "lf");
+        leftB = hardwareMap.get(DcMotorEx.class, "lr");
+        rightF = hardwareMap.get(DcMotorEx.class, "rf");
+        rightB = hardwareMap.get(DcMotorEx.class, "rr");
         pisun = hardwareMap.crservo.get("pis");
-        pod = hardwareMap.dcMotor.get("pod");
+        pod = hardwareMap.get(DcMotorEx.class, "pod");
         big = hardwareMap.crservo.get("big");
         zaxvat = hardwareMap.crservo.get("zx");
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEra/Utils/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewEra/Utils/PID.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.NewEra.Utils;
 
-import org.firstinspires.ftc.teamcode.NewEra.Phantom;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.teamcode.NewEra.Phantom;
+@Config
 public class PID extends Phantom {
-    public double P, I, D;
-    public double errorP, errorI, errorD;
+    public double P;
+    public double I;
+    public double D;
+    public double errorP;
+    public double errorI;
+    public double errorD;
 
 }
