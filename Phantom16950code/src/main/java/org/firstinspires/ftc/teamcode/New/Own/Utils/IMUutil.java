32c2aaad8ebd429b4f5bdb77425fff401cd43a5f
Chuvxjr <dankost2005@live.ru>
2024-05-16 18:22:59 +0300

    Renaming

--- Phantom16950code/src/main/java/org/firstinspires/ftc/teamcode/New/Own/Utils/IMUutil.java
+++ Phantom16950code/src/main/java/org/firstinspires/ftc/teamcode/New/Own/Utils/IMUutil.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.New.Own.Utils;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
-
+//
 public class IMUutil extends LinearOpMode {
     public void initIMU(){
 
